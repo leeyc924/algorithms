7184cb003bba6bd31962d86e90e1cd9b88824953
leeyc924 <leeyc924@naver.com>
2026-02-13 14:03:01 +0900

    refactor: 서버 증설 로직 개선 및 카운트 변수 명 변경

--- programmers/level2/서버증설횟수.js
+++ programmers/level2/서버증설횟수.js
@@ -2,29 +2,20 @@
  * https://school.programmers.co.kr/learn/courses/30/lessons/389479?language=javascript
  */
 function solution(players, m, k) {
-  let answer = 0; // 서버 증설횟수
-  const servers = new Array(players.length).fill(0);
-
+  const serverArr = Array.from({ length: 24 + k }).fill(0);
+  let increaseCnt = 0;
   for (let i = 0; i < players.length; i++) {
-    const playerCount = players[i];
-    // 서버한대가 처리할 수 있는 플레이어 수
-    const server = Number.parseInt(playerCount / m);
-    if (server > servers[i]) {
-      // 필요한 서버 수
-      const needServerCount = server - servers[i];
-
-      // 서버 증설
-      for (let j = 0; j < k; ++j) {
-        if (i + j < 24) {
-          // 서버 증설 후 서버 수 업데이트
-          servers[i + j] = servers[i + j] + needServerCount;
-        }
+    const player = players[i];
+    const needServer = Math.floor(player / m);
+    if (serverArr[i] < needServer) {
+      const increase = needServer - serverArr[i];
+      increaseCnt += increase;
+      for (let j = i; j < i + k; j++) {
+        serverArr[j] += increase;
       }
-      // 서버 증설횟수 증가
-      answer += needServerCount;
     }
   }
-  return answer;
+  return increaseCnt;
 }
 
 console.log(
